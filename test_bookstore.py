1519551a40b8fdb3bb0e0131df835e3864a5442f
Ivan <zugnoni.ivan@gmail.com>
2017-08-10 21:40:26 -0300

    Update test_bookstore.py

--- test_bookstore.py
+++ test_bookstore.py
@@ -6,4 +6,4 @@ from bookstore import Bookstore, Book, Author
 class BookstoreTestCase(unittest.TestCase):
 
     def test_instantiate_bookstore(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
